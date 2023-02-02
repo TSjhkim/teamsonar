f7e7f14c9eda8adb67fff58d2d7a13720c23f69f
jhkim0228@tangunsoft.com <jhkim0228@tangunsoft.com>
2023-02-02 14:53:56 +0900

    Commiiiiiiiiiit!

---
+++ sonar.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class sonar {
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String score = sc.next();
+
+            switch(score){
+                case "A":
+                    System.out.println("A등급 입니다");
+                    break;
+                case "B":
+                    System.out.println("B등급 입니다");
+                    break;
+                case "C":
+                    System.out.println("C등급 입니다");
+                    break;
+            }
+        }
+    }
+}
